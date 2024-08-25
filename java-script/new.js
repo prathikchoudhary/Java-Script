bd534f2e65a0676d6f2e34d2f6253b3848f3a78b
Prathik choudhary <136879319+prathikchoudhary@users.noreply.github.com>
2024-08-25 02:51:04 +0000

    new V0.02

--- java-script/new.js
+++ java-script/new.js
@@ -1,3 +1,21 @@
-let names = "prathik";
+let number = 0
 
-console.log(names);
+let value = Boolean(number);
+
+// console.log(value);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+
+// console.log(6 != 2);
+
+
+const id = Symbol("123456")
+const id2 = Symbol([123456])
+
+
+console.log(id2 === id)
